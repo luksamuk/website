20e5f4c585ec654294ec8ff976b53821b3a7b37d
Lucas Vieira <lucasvieira@protonmail.com>
2020-04-18 19:25:32 -0300

    Modify Index generation script

--- scripts/export_index.sh
+++ scripts/export_index.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-emacs --batch -l "~/.emacs.d/init.el" --kill src/index.org -f org-html-export-to-html
-mv src/index.html docs/index.html
+emacs --batch -l "~/.emacs.d/init.el" --kill "src/index.org" -f org-html-export-to-html
+mv src/index.html docs/
