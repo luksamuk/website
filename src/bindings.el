7efc8f85c313886e6fef9f45943a0f8fc9c63e22
Lucas S. Vieira <lucasvieira@protonmail.com>
2020-10-06 18:37:35 -0300

    Fix typo in export script

--- src/bindings.el
+++ src/bindings.el
@@ -1,5 +1,5 @@
 (setq org-html-preamble-format
-      '(("en" "<h1 class=\"title\">%t</h1>" "<p><i>Written in %d by %a<br/>%e</i></p>")
+      '(("en" "<h1 class=\"title\">%t</h1><p><i>Written in %d by %a<br/>%e</i></p>")
         ("pt_BR" "<h1 class=\"title\">%t</h1><p><i>Escrito em %d por %a<br/>%e</i></p>"))
       org-html-postamble-format
       '(("en" "<h3><a href=\"../\">Back to last page</a></h3>")
