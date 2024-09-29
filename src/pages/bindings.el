35b6d3d25bda83a127fc5e25258a559f47689d4a
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-09-29 19:22:31 -0300

    Fix pages

---
+++ src/pages/bindings.el
@@ -0,0 +1,6 @@
+(setq org-html-preamble-format
+      '(("en" "<nav><h1><a href=\"../\">The Alchemist's Hideout</a></h1></nav><h1 class=\"title\">%t</h1>")
+        ("pt_BR" "<nav><h1><a href=\"../\">The Alchemist's Hideout</a></h1></nav><h1 class=\"title\">%t</h1>"))
+      org-html-postamble-format
+      '(("en" "<h3><a href=\"../\">Back to last page</a></h3>")
+        ("pt_BR" "<h3><a href=\"../\">De volta à página anterior</a></h3>")))
