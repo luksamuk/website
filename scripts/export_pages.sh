d38cf0d076272aaa3026d5c433dca2b8acf31a49
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-09-30 12:42:11 -0300

    Fix homebrews page title

--- scripts/export_pages.sh
+++ scripts/export_pages.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 for f in `find src/pages -name "*.org"`; do
     echo "$f";
-    emacs --batch \
-	  -l "/root/.emacs.d/init.el" \
-	  -l "./src/pages/bindings.el" \
-	  --kill \
-	  "$f" \
-	  -f org-html-export-to-html;
+    if [ -f "${f%%.org}-bindings.el" ]; then
+	emacs --batch \
+	      -l "/root/.emacs.d/init.el" \
+	      -l "${f%%.org}-bindings.el" \
+	      --kill \
+	      "$f" \
+	      -f org-html-export-to-html;
+    else
+	emacs --batch \
+	      -l "/root/.emacs.d/init.el" \
+	      -l "./src/pages/bindings.el" \
+	      --kill \
+	      "$f" \
+	      -f org-html-export-to-html;
+    fi
     mv "${f%%.org}.html" docs/pages/;
     cp src/pages/*.cpp docs/pages;
 done
---
+++ src/pages/psx-homebrew-bindings.el
@@ -0,0 +1,6 @@
+(setq org-html-preamble-format
+      '(("en" "<nav><h1><a href=\"../\">The Alchemist's Hideout</a></h1></nav>")
+        ("pt_BR" "<nav><h1><a href=\"../\">The Alchemist's Hideout</a></h1></nav>"))
+      org-html-postamble-format
+      '(("en" "<h3><a href=\"../\">Back to last page</a></h3>")
+        ("pt_BR" "<h3><a href=\"../\">De volta à página anterior</a></h3>")))
